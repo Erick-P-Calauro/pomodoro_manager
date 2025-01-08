2eaa3c54838cfbef254a9f4296e40683ca074833
Erick-P-Calauro <erickcalauro@gmail.com>
2025-01-08 10:58:32 -0400

    newTarefa id assignment fix

--- src/ui/logic/tarefaReducer/reducer.ts
+++ src/ui/logic/tarefaReducer/reducer.ts
@@ -6,7 +6,7 @@ export const TarefaReducer = (state : Tarefa[], action) => {
     switch(type) {
         case "CADASTRAR_TAREFA" : {
             const newTarefa = action.payload;
-            newTarefa.id = state[state.length - 1].id + 1;
+            newTarefa.id = state.length !== 0 ? state[state.length - 1].id + 1 : 0;
 
             return [...state, newTarefa];
         }
