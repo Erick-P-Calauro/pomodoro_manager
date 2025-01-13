4cf753615786dfec35d8b7f6e74ed67f5908f6fc
Erick-P-Calauro <erickcalauro@gmail.com>
2025-01-13 14:41:42 -0400

    first commit : project structure

---
+++ src/main/java/com/erick/pomodoro_manager/PomodoroManagerApplication.java
@@ -0,0 +1,13 @@
+package com.erick.pomodoro_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroManagerApplication.class, args);
+	}
+
+}
